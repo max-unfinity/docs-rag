5d26a73693153cb7baf6d2fe3110b8d668482c8f
max-unfinity <poldersen2@gmail.com>
2024-01-08 07:51:29 +0000

    app dev

--- chat_app.py
+++ chat_app.py
@@ -1,13 +1,38 @@
 import streamlit as st
-from src.chromadb import read_db, get_retriever
-from src.chain import get_chain, run_chain
 from dotenv import load_dotenv
+import json
+import os
+from src.chromadb import read_db, get_retriever
+from src.chain import get_chain, retrieve_docs, generate_response, generate_response_stream
 
-k = st.sidebar.number_input("K retrieval", value=5, step=1, min_value=1, max_value=10)
+
+def get_sources(docs):
+    sources = []
+    for doc in docs:
+        base_url = st.session_state.config["base_url"]
+        docs_dir = st.session_state.config["docs_dir"]
+        base_path = doc.metadata.get("source")
+        base_path = os.path.splitext(base_path)[0]
+        base_path = base_path.replace(docs_dir, "")
+        if base_path[0] == "/":
+            base_path = base_path[1:]
+        if base_url[-1] == "/":
+            base_url = base_url[:-1]
+        url = f"{base_url}/{base_path}"
+        if base_path.endswith("README"):
+            url = url[:-7]
+        sources.append(url)
+    return sources
+
+
+k = st.sidebar.number_input("K retrieval", value=4, step=1, min_value=1, max_value=10)
 
 # Init
 if "vectorstore" not in st.session_state:
     load_dotenv(".env")
+    with open("config.json") as f:
+        config = json.load(f)
+    st.session_state.config = config
     st.session_state.vectorstore = read_db("supervisely-dev-portal-db")
     st.session_state.retriever = get_retriever(st.session_state.vectorstore, k=k)
     st.session_state.chain = get_chain(st.session_state.retriever)
@@ -19,42 +44,47 @@ if "k" in st.session_state and st.session_state.k != k:
     st.session_state.k = k
 
 
-st.title("Superivsely Dev Portal Chat")
+st.title("Superivsely SDK Assistant")
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
 
 # Accept user input
 prompt = st.chat_input("Type your message here...")
 if prompt:
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        # message_placeholder = st.empty()
-        assistant_response, docs = run_chain(st.session_state.chain, st.session_state.retriever, prompt)
-        for doc in docs:
-            st.info(doc.metadata.get("source"))
-            st.markdown(doc.page_content[:500])
-            st.markdown("...")
-        st.markdown(assistant_response)
-        full_response = assistant_response
+        docs = retrieve_docs(st.session_state.retriever, prompt)
+        sources = get_sources(docs)
+        references = "\n".join([f"- {source}" for source in sources])
+        references = "**Retrieved docs:**\n" + references
+        st.markdown(references)
+
+        # response = generate_response(st.session_state.chain, prompt)
+        # response = "**Answer:**\n\n" + response
+        # st.markdown(response)
+
         # Simulate stream of response with milliseconds delay
-        # for chunk in assistant_response.split():
-        #     full_response += chunk + " "
-        #     time.sleep(0.1)
-        #     # Add a blinking cursor to simulate typing
-        #     message_placeholder.markdown(full_response + "▌")
-        # message_placeholder.markdown(full_response)
+        response = ""
+        message_placeholder = st.empty()
+        for chunk in generate_response_stream(st.session_state.chain, prompt):
+            response += chunk
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(response + "▌")
+        message_placeholder.markdown(response)
+    
+    response = references + "\n\n" + response
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    st.session_state.messages.append({"role": "assistant", "content": response})
--- src/chain.py
+++ src/chain.py
@@ -1,7 +1,7 @@
 from operator import itemgetter
 from typing import Sequence
 
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.document import Document
 from langchain.schema.output_parser import StrOutputParser
@@ -25,15 +25,32 @@ def format_docs(docs: Sequence[Document]) -> str:
 
 
 def get_chain(retriever, model="gpt-3.5-turbo-1106", temperature=0.7):
+    # prompt = ChatPromptTemplate.from_messages(
+    #     [
+    #         (
+    #             "system",
+    #             "Here are some documents from Supervisely Developer docs to expand your knowledge about the topic:"
+    #             "\n{context}\n"
+    #             "Carefully respond to the user query using the provided documents."
+    #             "If provided documents are not relevant, mention it to the user and answer based on your knowledge.",
+    #         ),
+    #         ("human", "{question}"),
+    #     ]
+    # )
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                "Here are some documents from Supervisely Developer docs to expand your knowledge about the topic:"
-                "\n{context}\n"
-                "Carefully respond to the user query using the provided documents. It there is no relevant information, mention it to the user and try to answer based on your knowledge.",
+                "You are a specialized assistant focused to help users with Supervisely Python SDK. You are adept at providing coding assistance and answering queries related to the SDK. You will be provided with parts of the Supervisely documentation. Incorporate texts from Supervisely documentation into your responses, offering enriched explanations when necessary. You handle multiple documents adeptly, synthesizing information to deliver comprehensive, accurate answers. You maintain a strictly professional and technical tone, ensuring responses are clear and precise, making it suitable for both beginners and seasoned users of the SDK. Avoid misinformation. In situations involving ambiguous or partial questions, you either seek clarification or make logical assumptions to offer useful guidance. The primary focus is to be a reliable resource for all things related to the Supervisely Python SDK, ensuring users receive expert advice and code examples tailored to their needs.",
             ),
-            ("human", "{question}"),
+            (
+                "human",
+                """Here are texts from Superivsely SDK documentation:
+                {context}
+
+                Now, the question is:
+                {question}"""
+            ),
         ]
     )
     llm = ChatOpenAI(temperature=temperature, model=model)
@@ -63,7 +80,17 @@ def get_chain(retriever, model="gpt-3.5-turbo-1106", temperature=0.7):
     return chain
 
 
-def run_chain(chain, retriever, prompt):
-    response = chain.invoke({"question": prompt})
+def retrieve_docs(retriever, prompt):
     docs = retriever.invoke(prompt)
-    return response, docs
+    return docs
+
+
+def generate_response(chain, prompt):
+    response = chain.invoke({"question": prompt})
+    return response
+
+
+
+def generate_response_stream(chain, prompt):
+    response_stream = chain.stream({"question": prompt})
+    return response_stream
